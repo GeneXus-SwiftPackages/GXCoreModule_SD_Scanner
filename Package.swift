744251b5f98cca196948d10e94aea2b36b7a9a67
Build Er <builder@macmini6.local>
2023-05-24 15:56:29 -0300

    Package version: 1.0.0-beta+20230524184838

--- Package.swift
+++ Package.swift
@@ -29,8 +29,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Scanner",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Scanner-1.0.0-beta+20230519205149.xcframework.zip",
-			checksum: "a9fc8b7188011ca9c4395f2f1044669fa0bf52183d173ba6c04d62b0be16e79b"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Scanner-1.0.0-beta+20230524184838.xcframework.zip",
+			checksum: "0b8663ec577a059e93ddfeb8889dab57f8593913fa07705baed369e0bab31e0b"
 		)
 	]
 )
