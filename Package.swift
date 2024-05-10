41cf46b2eac97d558a25f17c3e0a818bd61cd4e6
Build Er <builder@macmini6.local>
2024-05-10 07:46:28 -0300

    Package version: 2.0.0-beta.28

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_ScannerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.27")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.28")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_ScannerWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Scanner",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Scanner-2.0.0-beta.27.xcframework.zip",
-			checksum: "681e9fe017d0e4700dee8cf7702e21aafd91cb4da148d9ced9f25a9c56200fea"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Scanner-2.0.0-beta.28.xcframework.zip",
+			checksum: "0ecffaa78bf76f52f004a266c5c2a249d05c9d8d5026cea7b4d6fec7303cf9c9"
 		)
 	]
 )
