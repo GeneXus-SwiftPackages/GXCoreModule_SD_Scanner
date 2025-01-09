56ed91fa4c260ca29f2b97f06df5e1d758482909
The iOS Team @ GeneXus <builder@genexus.com>
2025-01-09 14:31:20 -0300

    Package version: 3.0.0-rc.2

--- Package.swift
+++ Package.swift
@@ -1,36 +1,28 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXCoreModule_SD_Scanner",
-	platforms: [.iOS("12.0")],
+	platforms: [.iOS("13.0"), .visionOS("1.0")],
 	products: [
 		.library(
 			name: "GXCoreModule_SD_Scanner",
 			targets: ["GXCoreModule_SD_ScannerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.0.0-rc.2")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_ScannerWrapper",
 				dependencies: [
 					"GXCoreModule_SD_Scanner",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
-					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.iOS])),
-					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
-					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Scanner",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Scanner-1.1.0.xcframework.zip",
-			checksum: "16f0ab1b81b56704f3f7bc3d8c5fd7de7cee0e44b11a7a596954e2e2340b9fc9"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXCoreModule_SD_Scanner-3.0.0-rc.2.xcframework.zip",
+			checksum: "fae47925839628d59f0a7c4c48293cd03cff0b5d37cd7bfa2ddeccaa39492a9a"
 		)
 	]
 )
